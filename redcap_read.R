3408ce03027b40180cfeee521e50cac0f43a6521
tfettrow <tfettrow@gmail.com>
2021-09-27 15:04:43 -0400

    Update redcap_read.R

--- redcap_read.R
+++ redcap_read.R
@@ -12,5 +12,6 @@ all_data_frame$fmri_zero_short_dprime
 
 subject_id_prefix_pattern <- paste(c("H","NH"), collapse = '|')
 subjects_of_interest_indices <- grep(subject_id_prefix_pattern,all_data_frame$record_id)
-all_data_frame$record_id[subjects_of_interest_indices]
+
+this_pattern_did_not_work <- all_data_frame$record_id[subjects_of_interest_indices]
 
