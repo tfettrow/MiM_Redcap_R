69c64465593012bea2f194c5f0829f88748d75c1
tfettrow <tfettrow@gmail.com>
2021-09-27 14:54:02 -0400

    Create redcap_read.R

---
+++ redcap_read.R
@@ -0,0 +1,16 @@
+library(REDCapR)
+
+
+#token <- "6E6CDFE57EF460564AE0F180BCCE734B"  # TF token
+
+token <- "2306C4C09DE54073D5040ECAB415861A"  # AT token
+
+
+url <- "https://redcap.ctsi.ufl.edu/redcap/api/"
+all_data_frame <- redcap_read(redcap_uri = url, token = token)$data
+all_data_frame$fmri_zero_short_dprime
+
+subject_id_prefix_pattern <- paste(c("H","NH"), collapse = '|')
+subjects_of_interest_indices <- grep(subject_id_prefix_pattern,all_data_frame$record_id)
+all_data_frame$record_id[subjects_of_interest_indices]
+
