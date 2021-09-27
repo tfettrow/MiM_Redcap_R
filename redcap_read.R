b05d477005d97498fad153fbe2adb8d491b706c5
atouheed <atouheed@ufl.edu>
2021-09-27 15:49:18 -0400

    Update redcap_read.R
    I changed the pattern for the indices to specify the subjects we want to look at and then the mini data frame we made is added as well

--- redcap_read.R
+++ redcap_read.R
@@ -10,8 +10,8 @@ url <- "https://redcap.ctsi.ufl.edu/redcap/api/"
 all_data_frame <- redcap_read(redcap_uri = url, token = token)$data
 all_data_frame$fmri_zero_short_dprime
 
-subject_id_prefix_pattern <- paste(c("H","NH"), collapse = '|')
+subject_id_prefix_pattern <- paste(c("H1","H2","H3","NH"), collapse = '|')
 subjects_of_interest_indices <- grep(subject_id_prefix_pattern,all_data_frame$record_id)
 
 this_pattern_did_not_work <- all_data_frame$record_id[subjects_of_interest_indices]
-xkjhfkjfk
+sppb_data_frame=data.frame(all_data_frame$record_id[subjects_of_interest_indices], all_data_frame$fmri_zero_short_dprime[subjects_of_interest_indices])
