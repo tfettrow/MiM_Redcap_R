6469c0559fb460d3451ec90f225d9889c8679d65
tfettrow <tfettrow@gmail.com>
2021-10-12 14:17:20 -0400

    Update missing_data_heatmap.R

--- missing_data_heatmap.R
+++ missing_data_heatmap.R
@@ -1,25 +1,42 @@
 library(REDCapR)
+library(ggplot2)
 
-
-#token <- "6E6CDFE57EF460564AE0F180BCCE734B"  # TF token
-
-token <- "2306C4C09DE54073D5040ECAB415861A"  # AT token
-
-
+token <- "6E6CDFE57EF460564AE0F180BCCE734B"  # TF token
 url <- "https://redcap.ctsi.ufl.edu/redcap/api/"
 all_data_frame <- redcap_read(redcap_uri = url, token = token)$data
-all_data_frame$fmri_zero_short_dprime
 
-subject_id_prefix_pattern <- paste(c("H1","H2","H3","NH"), collapse = '|')
+#token <- "2306C4C09DE54073D5040ECAB415861A"  # AT token
+variables_of_interest <- c("mri_checklist_complete","white_matter_integrity_complete", "grey_matter_volume_complete","mri_fmri_brain_imagined_treadmill_complete","mri_fmri_brain_n_back_complete","conn_complete","imagined_treadmill_walking_test_fmri_complete","mri_nback_performance_complete")
+# variables_of_interest <- c("zero_short_dprime")
+
+
+subject_id_prefix_pattern <- paste(c("H1", "H2", "H3","NH1","NH2","NH3"), collapse = '|')
+
 subjects_of_interest_indices <- grep(subject_id_prefix_pattern,all_data_frame$record_id)
+# session_of_interest_indices <- which(all_data_frame$redcap_event_name=="screening_visit_arm_1")
+session_of_interest_indices <- which(all_data_frame$redcap_event_name=="base_v4_mri_arm_1")
 
-#this_pattern_did_not_work <- all_data_frame$record_id[subjects_of_interest_indices]
-sppb_data_frame=data.frame(all_data_frame$record_id[subjects_of_interest_indices], all_data_frame$fmri_zero_short_dprime[subjects_of_interest_indices])
-#this did not work as it creates data frame for all variables to be true and false
-sppb_data_frame_2= data.frame(is.na(sppb_data_frame$all_data_frame.fmri_zero_short_dprime.subjects_of_interest_indices))
-#only creates data frame for the variable of interest 
-is.na(sppb_data_frame$all_data_frame.fmri_zero_short_dprime.subjects_of_interest_indices.)
-#save this for later
-sppb_data_frame_filter[data.frame(all_data_frame$record_id[subjects_of_interest_indices], all_data_frame$fmri_zero_short_dprime[subjects_of_interest_indices]) %in% c("NA")]
- 
+for (this_variable in variables_of_interest)
+  {
+    # this_variable_of_interest_indices <- which(!is.na(eval(parse(text =paste0('all_data_frame$',this_variable)))))
+    # this_subject_variable_overlap_indices <- intersect(intersect(subjects_of_interest_indices,this_variable_of_interest_indices),session_of_interest_indices)
+    # this_variable_dataframe <- data.frame(all_data_frame$record_id[this_subject_variable_overlap_indices],eval(parse(text =paste0('all_data_frame$',this_variable)))[this_subject_variable_overlap_indices])
+
+    # this_variable_of_interest_binary_array <- as.integer(!is.na(eval(parse(text =paste0('all_data_frame$',this_variable)))))
+    this_variable_of_interest_binary_array <- eval(parse(text =paste0('all_data_frame$',this_variable)))
+    this_variable_of_interest_binary_array[is.na(this_variable_of_interest_binary_array)] <- 0
+    this_subject_session_overlap_indices <- intersect(subjects_of_interest_indices,session_of_interest_indices)
+
+    record_id_number_only <- gsub("[^0-9.-]", "", all_data_frame$record_id)
+
+
+    this_variable_of_interest_filtered_data_frame <- data.frame(record_id_number_only[this_subject_session_overlap_indices], this_variable_of_interest_binary_array[this_subject_session_overlap_indices])
+
+
+    colnames(this_variable_of_interest_filtered_data_frame) <- c("record_id",this_variable)
+
+    ggplot(this_variable_of_interest_filtered_data_frame, aes(mri_checklist_complete,record_id)) +
+      geom_tile()
+  }
+
 
