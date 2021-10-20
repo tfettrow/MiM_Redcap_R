2a698aae14c8fa6ddbdd83351ab6b3b5b52613c8
atouheed <atouheed@ufl.edu>
2021-10-19 23:01:43 -0400

    Update missing_data_heatmap.R
    fixed separation issues with number and font

--- missing_data_heatmap.R
+++ missing_data_heatmap.R
@@ -35,7 +35,7 @@ for (this_variable in variables_of_interest)
     ggplot(this_variable_of_interest_filtered_data_frame, aes_string(this_variable,record_id)) +
       geom_tile() +
       theme(
-        axis.text.y = element_text(size=8.75, hjust=-1.5, margin = margin(1,1,1,1,"pt")),
+        axis.text.y = element_text(size=8.5, hjust=3, vjust=.5, margin = margin(2,1,1,1,"cm")),
         panel.background = element_rect(fill="white", color= "grey")
         )
        #this does not work at all 
