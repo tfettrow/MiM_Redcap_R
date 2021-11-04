e700c46819aec0e4e203171a410d277de948b341
atouheed <atouheed@ufl.edu>
2021-11-04 16:49:31 -0400

    Update copy_MiM_data.R
    figured out how to make R interact with google drive

--- copy_MiM_data.R
+++ copy_MiM_data.R
@@ -1,17 +1,15 @@
-current_folder = "/Volumes/blue/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data" #AT Mac File path 
-#new_folder= "/Volumes/GoogleDrive/Shared\ drives/MiM_Backup"  #AT Mac file path 
-new_folder= getwd()
+library(googledrive)
+current_folder = "Volumes/blue/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data" #AT Mac File path 
+new_folder= "https://drive.google.com/drive/u/1/folders/0APPdHWQoH9zKUk9PVA"  #Google drive link for shared drive
 list_of_subjects <- c("1002")
 #actual_list_of_subjects= "1004", "1007", "1009", "1010", "1011", "1012", "1013", "1017", "1018", "1019", "1020", "1022", "1024", "1025", "1026", "3066 1027 2002 2007 2008 2012 2013 2015 2017 2018 2020 2021  2022 2023 2025 2026 2027 2033 20342037203820392042205220592062300430063007300830103021302330243025302630273028302930303034303630393040304230433046304730513053305830593063
 for (this_subject in list_of_subjects)
 {
-  dir.create(file.path(this_subject,"Raw"))
+  drive_mkdir(this_subject, path = "https://drive.google.com/drive/u/1/folders/0APPdHWQoH9zKUk9PVA")
+   #successfully creates the subject folder 
+   #Don't know how to include subject folder with subfolder
   file.copy(file.path(current_folder,this_subject,"Raw"), file.path(new_folder,this_subject,"Raw"), recursive= TRUE)
+  #not sure how to execute this function in copying from one network drive to another
 }
-#Warning messages (these errors pull up everytime while running)
-# 1: In dir.create(file.path(this_subject, "Raw")) :
-#cannot create dir '1002/Raw', reason 'No such file or directory'
-#2: In file.copy(file.path(current_folder, this_subject, "Raw"), file.path(new_folder,  :
-#'recursive' will be ignored as 'to' is not a single existing directory
-#'# 3: In file.create(to[okay]) : cannot create file '/Volumes/GoogleDrive/Shared drives/MiM_Backup/1002/Raw', reason 'No such file or directory'
+
                                                                           
