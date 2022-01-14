3a9d259fbca9d075ecba8439015321d3904e79e4
valay93 <69319816+valay93@users.noreply.github.com>
2022-01-14 15:25:57 -0500

    some functions for MiM

--- BehaviorBoxplots.R
+++ BehaviorBoxplots.R
@@ -1,3 +1,15 @@
-Red_NIH<-as.data.frame(NIH[,c(22,23,4,9,10,15,16)])
+Red_NIH<-as.data.frame(NIH[,c(8,10,3:7)])
 l<-reshape(data=Red_NIH,varying=c("wm_rs","dc_rs","dc_cs","flanker_rs","fi_cs"),v.names="score",timevar = "test",times=c("wm_rs","dc_rs","dc_cs","flanker_rs","fi_cs"),new.row.names = 1:620,direction = "long")
-boxplot(score~grp*test,data=l,las=2)
+l$test[l$test=="wm_rs"]<-"WMRaw"
+l$test[l$test=="dc_cs"]<-"DemCardComputed"
+l$test[l$test=="fi_cs"]<-"FlankerComputed"
+# boxplot(score~grp*test,data=l,las=3,main='NIHtoolbox Scores by age grp')
+#pairs(l$test)
+names(Red_NIH)[3]<-"WMRaw"
+names(Red_NIH)[5]<-"DemCardComputed"
+names(Red_NIH)[7]<-"FlankerComputed"
+pairs(Red_NIH$WMRaw+Red_NIH$DemCardComputed+Red_NIH$FlankerComputed+jitter(Conc$MFT,3)+jitter(Conc$TPT,3)~Red_NIH$grp,log=4,col = c("blue", "red", "green")[Red_NIH$grp],labels=c("WMRaw","DemCardComp","FlankerComp","MicroFil (g)","TwoPTDisc (mm)"),lower.panel=NULL)
+legend(x="bottomleft",fill=c("blue", "red", "green"), legend=c("1","2","3"))
+
+ppp<-as.data.frame(reshape(data=gaitdata,varying=c("DelGait","DelOG","VBL","OgHigh"),times=c("DelGait","DelOG","VBL","OgHigh"), direction = "long",v.names="value",timevar = "times",new.row.names = 1:124))
+boxplot(gaitdata$delta_OGgaitdata$delta_Gait~gaitdata$grp)
--- CopyNihData.R
+++ CopyNihData.R
@@ -9,29 +9,29 @@ for (val in x) {
   print(val)
   #if (any(which(NIH_data$PIN == as.character(val))) & )
   #{
-  if (any(with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1"))){
-    NIH$wm_rs[NIH$record_id==as.character(val)]<-NIH_data$RawScore[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$wm_uss[NIH$record_id==as.character(val)]<-NIH_data$`Uncorrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$wm_acss[NIH$record_id==as.character(val)]<-NIH_data$`Age-Corrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$wm_np[NIH$record_id==as.character(val)]<-NIH_data$`National Percentile (age adjusted)`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$wm_fcts[NIH$record_id==as.character(val)]<-NIH_data$`Fully-Corrected T-score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
-  }
-  if (any(with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1"))){
-    NIH$dc_rs[NIH$record_id==as.character(val)]<-NIH_data$RawScore[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$dc_cs[NIH$record_id==as.character(val)]<-NIH_data$`Computed Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$dc_uss[NIH$record_id==as.character(val)]<-NIH_data$`Uncorrected Standard Score`[with(NIH_data,PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$dc_acss[NIH$record_id==as.character(val)]<-NIH_data$`Age-Corrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$dc_np[NIH$record_id==as.character(val)]<-NIH_data$`National Percentile (age adjusted)`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$dc_fct[NIH$record_id==as.character(val)]<-NIH_data$`Fully-Corrected T-score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-  }
-  if (any(with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1"))){
-    NIH$flanker_rs[NIH$record_id==as.character(val)]<-NIH_data$RawScore[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$fi_cs[NIH$record_id==as.character(val)]<-NIH_data$`Computed Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$fi_uss[NIH$record_id==as.character(val)]<-NIH_data$`Uncorrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$fi_acss[NIH$record_id==as.character(val)]<-NIH_data$`Age-Corrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$fi_np[NIH$record_id==as.character(val)]<-NIH_data$`National Percentile (age adjusted)`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-    NIH$fi_fcts[NIH$record_id==as.character(val)]<-NIH_data$`Fully-Corrected T-score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
-  }
+  # if (any(with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1"))){
+  #   NIH$wm_rs[NIH$record_id==as.character(val)]<-NIH_data$RawScore[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$wm_uss[NIH$record_id==as.character(val)]<-NIH_data$`Uncorrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$wm_acss[NIH$record_id==as.character(val)]<-NIH_data$`Age-Corrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$wm_np[NIH$record_id==as.character(val)]<-NIH_data$`National Percentile (age adjusted)`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$wm_fcts[NIH$record_id==as.character(val)]<-NIH_data$`Fully-Corrected T-score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox List Sorting Working Memory Test Age 7+ v2.1" & `Assessment Name`=="Assessment 1")]
+  # }
+  # if (any(with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1"))){
+  #   NIH$dc_rs[NIH$record_id==as.character(val)]<-NIH_data$RawScore[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$dc_cs[NIH$record_id==as.character(val)]<-NIH_data$`Computed Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$dc_uss[NIH$record_id==as.character(val)]<-NIH_data$`Uncorrected Standard Score`[with(NIH_data,PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$dc_acss[NIH$record_id==as.character(val)]<-NIH_data$`Age-Corrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$dc_np[NIH$record_id==as.character(val)]<-NIH_data$`National Percentile (age adjusted)`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$dc_fct[NIH$record_id==as.character(val)]<-NIH_data$`Fully-Corrected T-score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Dimensional Change Card Sort Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  # }
+  # if (any(with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1"))){
+  #   NIH$flanker_rs[NIH$record_id==as.character(val)]<-NIH_data$RawScore[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$fi_cs[NIH$record_id==as.character(val)]<-NIH_data$`Computed Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$fi_uss[NIH$record_id==as.character(val)]<-NIH_data$`Uncorrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$fi_acss[NIH$record_id==as.character(val)]<-NIH_data$`Age-Corrected Standard Score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$fi_np[NIH$record_id==as.character(val)]<-NIH_data$`National Percentile (age adjusted)`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  #   NIH$fi_fcts[NIH$record_id==as.character(val)]<-NIH_data$`Fully-Corrected T-score`[with(NIH_data, PIN==as.character(val) & Inst=="NIH Toolbox Flanker Inhibitory Control and Attention Test Age 12+ v2.1" & `Assessment Name`=="Assessment 1")]
+  # }
   ifelse (!is.na(NIH$wm_rs[NIH$record_id==as.character(val)]), NIH$nih_toolbox_assessment_scores_complete[NIH$record_id==as.character(val)]<-2, NIH$nih_toolbox_assessment_scores_complete[NIH$record_id==as.character(val)]<-0)
   ifelse (NIH$add[NIH$record_id==as.character(val)]<2000, NIH$grp[NIH$record_id==as.character(val)]<-1, NIH$grp[NIH$record_id==as.character(val)]<-2)
   if (NIH$add[NIH$record_id==as.character(val)]>3000) {NIH$grp[NIH$record_id==as.character(val)]<-3}
@@ -40,25 +40,35 @@ for (val in x) {
 }
 
 #n<-n+1
-x<-X2ptDiscScores$record_id
-X2ptDiscScores$add<-X2ptDiscScores$record_id
-X2ptDiscScores$add<-gsub("H","",as.character(X2ptDiscScores$add))
-X2ptDiscScores$add<-gsub("N","",as.character(X2ptDiscScores$add))
-X2ptDiscScores$add<-as.numeric(X2ptDiscScores$add)
+x<-TPT$record_id
+TPT$add<-TPT$record_id
+TPT$add<-gsub("H","",as.character(TPT$add))
+TPT$add<-gsub("N","",as.character(TPT$add))
+TPT$add<-as.numeric(TPT$add)
 for (val in x) {
-  ifelse (X2ptDiscScores$add[X2ptDiscScores$record_id==as.character(val)]<2000, X2ptDiscScores$grp[X2ptDiscScores$record_id==as.character(val)]<-1, X2ptDiscScores$grp[X2ptDiscScores$record_id==as.character(val)]<-2)
-  if (X2ptDiscScores$add[X2ptDiscScores$record_id==as.character(val)]>3000) {X2ptDiscScores$grp[X2ptDiscScores$record_id==as.character(val)]<-3}
+  ifelse (TPT$add[TPT$record_id==as.character(val)]<2000, TPT$grp[TPT$record_id==as.character(val)]<-1, TPT$grp[TPT$record_id==as.character(val)]<-2)
+  if (TPT$add[TPT$record_id==as.character(val)]>3000) {TPT$grp[TPT$record_id==as.character(val)]<-3}
 }
-boxplot(X2ptDiscScores$somat_2pt_score~X2ptDiscScores$grp,las=2)
+boxplot(TPT$somat_2pt_score~TPT$grp,las=2,main="2pt disc scores by grp",xlab="grp",ylab="2pt disc distance (mm)")
 
 
-x<-MicroFilamentScores$record_id
-MicroFilamentScores$add<-MicroFilamentScores$record_id
-MicroFilamentScores$add<-gsub("H","",as.character(MicroFilamentScores$add))
-MicroFilamentScores$add<-gsub("N","",as.character(MicroFilamentScores$add))
-MicroFilamentScores$add<-as.numeric(MicroFilamentScores$add)
+x<-MFT$record_id
+MFT$add<-MFT$record_id
+MFT$add<-gsub("H","",as.character(MFT$add))
+MFT$add<-gsub("N","",as.character(MFT$add))
+MFT$add<-as.numeric(MFT$add)
 for (val in x) {
-  ifelse (MicroFilamentScores$add[MicroFilamentScores$record_id==as.character(val)]<2000, MicroFilamentScores$grp[MicroFilamentScores$record_id==as.character(val)]<-1, MicroFilamentScores$grp[MicroFilamentScores$record_id==as.character(val)]<-2)
-  if (MicroFilamentScores$add[MicroFilamentScores$record_id==as.character(val)]>3000) {MicroFilamentScores$grp[MicroFilamentScores$record_id==as.character(val)]<-3}
+  ifelse (MFT$add[MFT$record_id==as.character(val)]<2000, MFT$grp[MFT$record_id==as.character(val)]<-1, MFT$grp[MFT$record_id==as.character(val)]<-2)
+  if (MFT$add[MFT$record_id==as.character(val)]>3000) {MFT$grp[MFT$record_id==as.character(val)]<-3}
 }
-boxplot((as.numeric(MicroFilamentScores$somat_filament_score))~MicroFilamentScores$grp,las=2)
+MFT$somat_filament_score[MFT$somat_filament_score=="007"]<-0.07
+MFT$somat_filament_score[MFT$somat_filament_score=="04"]<-0.4
+#MFT$somat_filament_score[MFT$somat_filament_score=="02"]<-0.2
+#MFT$somat_filament_score[MFT$somat_filament_score=="7"]<-0.07
+MFT$somat_filament_score<-as.numeric(MFT$somat_filament_score)
+boxplot((MFT$somat_filament_score)~MFT$grp,las=2,main="Microfilament scores by grp",ylab="final microfilament",xlab="grp",log="y")
+
+Conc<-data.frame(TPT$add,TPT$grp,TPT$somat_2pt_score,MFT$somat_filament_score)
+names(Conc)<-c("add","grp","TPT","MFT")
+
+
